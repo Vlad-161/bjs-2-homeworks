dcf656cecb3efca19425f4eb148b6560d7a5b2fb
Vlad-161 <polovinkinvlaaad@gmail.com>
2025-06-21 16:15:19 +0300

    HW task2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let result;
+	let descr = (Math.pow(b, 2) - 4 * a * c);
+	if (descr === 0) {
+		result = -b / (2 * a);
+		arr = [result]
+	}
+	if (descr > 0) {
+		let first = (-b + Math.sqrt(descr)) / (2 * a);
+		let second = (-b - Math.sqrt(descr)) / (2 * a);
+		arr = [first, second]
+	}
+	return arr;
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let p = percent / 100 / 12;
+	let s = amount - contribution;
+	let monthPayment = s * (p + (p / (((1 + p) ** countMonths) - 1)));
+	let total = monthPayment * countMonths;
+
+	return Number(total.toFixed(2));
 }
