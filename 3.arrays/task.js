595dad18fcba2db34684a200288c7d65742b95ff
Vlad-161 <polovinkinvlaaad@gmail.com>
2025-06-29 18:30:45 +0300

    HW3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
+"use strict";
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length &&
+     arr1.every((element, index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const filteredUsers = users.filter(user => user.gender === gender);
+  return filteredUsers.length === 0
+    ? 0
+    : filteredUsers.reduce((sum, user) => sum + user.age, 0) / filteredUsers.length;
 }
